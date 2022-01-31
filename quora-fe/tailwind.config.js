758479faeda4b79f8af2f9b58de54d5eaee4f2c7
ankit-tailor <tailorankit3345@gmail.com>
2022-01-31 22:59:22 +0530

    feat: configure tailwind

---
+++ quora-fe/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      primaryDark: "#1D222E",
+      secondaryDark: "#151A26",
+      primaryLight: "#FFFEFE",
+      secondaryLight: "EFF1FD",
+      indigo: "#60A5FA",
+      lightGray: "#9CA3AF",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
