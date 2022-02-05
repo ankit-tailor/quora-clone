7bd2d61b5ef3b8ab608e79b5b2ecf3d54fa91d39
ankit-tailor <tailorankit3345@gmail.com>
2022-02-05 09:54:41 +0530

    feat: 404 page

---
+++ quora-fe/pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-indigo text-3xl font-bold underline">Not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
