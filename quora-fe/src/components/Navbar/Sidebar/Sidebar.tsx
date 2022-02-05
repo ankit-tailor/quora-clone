25d45f9c49b3584a5d3504d455423a69a062eff5
ankit-tailor <tailorankit3345@gmail.com>
2022-02-05 19:29:58 +0530

    fix: performance optimization

--- quora-fe/src/components/Navbar/Sidebar/Sidebar.tsx
+++ quora-fe/src/components/Navbar/Sidebar/Sidebar.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import { FcRules } from "react-icons/fc";
-// import { Routes } from "../../utils/contstants";
 import { NavbarItem } from "../NavbarItem/NavbarItem";
 import { Routes } from "../../../utils/constatns";
 
-export const Sidebar = () => {
+export const Sidebar = React.memo(function Sidebar() {
   return (
     <div className="hidden sm:block shadow-xl font-extrabold md:w-auto  lg:w-1/5 w-1/5 h-screen p-6 dark:bg-primaryDark bg-primaryLight dark:text-primaryLight text-primaryDark">
       <Link href="/">
         <a>
           <div className="brand flex items-center sm:mx-4 my-14">
             <FcRules
-            // className="text-5xl"
+              // @ts-ignore
+              className="text-5xl"
             />
             <h1 className="hidden lg:block text-indigo text-2xl mx-4">Quora</h1>
           </div>
@@ -26,4 +26,4 @@ export const Sidebar = () => {
       <hr className="hidden lg:block my-6 mx-8 text-lightGray border-t-2" />
     </div>
   );
-};
+});
