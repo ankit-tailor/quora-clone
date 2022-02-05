b994132c17dde760ad165506c2265bb67d986be6
ankit-tailor <tailorankit3345@gmail.com>
2022-02-05 09:54:31 +0530

    feat: profile page

---
+++ quora-fe/pages/profile.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Profile: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-indigo text-3xl font-bold underline">Profile page</h1>
+    </div>
+  );
+};
+
+export default Profile;
