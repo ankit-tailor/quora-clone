bddf3ce82ff0494f586a081e30ba61adbec92969
ankit-tailor <tailorankit3345@gmail.com>
2022-02-05 09:54:21 +0530

    feat: setting page

---
+++ quora-fe/pages/setting.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Setting: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-indigo text-3xl font-bold underline">
+        Settings page
+      </h1>
+    </div>
+  );
+};
+
+export default Setting;
