c3c775353d8898946c97f8b0f9cea5016dbf03f3
ankit-tailor <tailorankit3345@gmail.com>
2022-02-05 09:54:53 +0530

    feat: bookmark page

---
+++ quora-fe/pages/bookmark.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Bookmark: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-indigo text-3xl font-bold underline">
+        Bookmarks page
+      </h1>
+    </div>
+  );
+};
+
+export default Bookmark;
