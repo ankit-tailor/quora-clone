eeb4de195095d802a417d876c3a479fe7d7cdf67
ankit-tailor <tailorankit3345@gmail.com>
2022-02-07 10:25:06 +0530

    fix: secondaryLight color

--- quora-fe/tailwind.config.js
+++ quora-fe/tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
       primaryDark: "#1D222E",
       secondaryDark: "#151A26",
       primaryLight: "#FFFEFE",
-      secondaryLight: "EFF1FD",
+      secondaryLight: "#f1f1f1",
       indigo: "#60A5FA",
       lightGray: "#9CA3AF",
     },
