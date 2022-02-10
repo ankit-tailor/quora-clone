fb6f39c77b0a10cc578381f0a4c527fb1addcacd
ankit-tailor <tailorankit3345@gmail.com>
2022-02-10 23:05:51 +0530

    feat: add icons for dark / light mode

--- quora-fe/pages/settings.tsx
+++ quora-fe/pages/settings.tsx
@@ -3,26 +3,43 @@ import { useContext } from "react";
 import { Button } from "../src/components/button/Button";
 import { Layout } from "../src/components/Layout/Layout";
 import { ThemeContext } from "../src/providers/ThemeProvider";
+import { BiArrowBack, BiMoon, BiSun } from "react-icons/bi";
+import { useRouter } from "next/router";
 
 const Setting: NextPage = () => {
+  // @ts-ignore
   const { theme, toggleTheme } = useContext(ThemeContext);
-  console.log(theme, toggleTheme);
+  const router = useRouter();
 
   return (
     <div className="dark:bg-primaryDark bg-primaryLight">
       <Layout>
-        <div className="p-6 sm:px-10 sm:py-10 overflow-x-hidden h-screen w-screen mx-auto shadow-inner dark:bg-secondaryDark text-primaryLight bg-secondaryLight">
-          <h1 className="text-indigo text-3xl font-bold underline">
-            Settings page
-          </h1>
-          <Button
-            variant="primary"
-            color="indigo"
-            className="sm:inline my-2 py-2 px-2 sm:px-6 rounded-md shadow-2xl"
-            onClick={toggleTheme}
-          >
-            Dark mode
-          </Button>
+        <div className="p-6 sm:px-10 sm:py-10 overflow-x-hidden h-screen w-screen mx-auto shadow-inner dark:bg-secondaryDark dark:text-primaryLight text-primaryDark bg-secondaryLight">
+          <div className="flex items-center">
+            <BiArrowBack
+              //@ts-ignore
+              onClick={() => router.push("/")}
+              className="mx-4 cursor-pointer text-3xl text-indigo"
+            />
+            <h1 className="dark:text-secondaryLight text-secondaryDark text-2xl font-bold">
+              Settings
+            </h1>
+          </div>
+          <section className="p-6 my-6 shadow-xl w-full h-auto rounded-xl dark:bg-primaryDark bg-primaryLight dark:text-primaryLight text-primaryDark">
+            {theme === "light" ? (
+              <BiMoon
+                // @ts-ignore
+                className="cursor-pointer text-4xl"
+                onClick={toggleTheme}
+              />
+            ) : (
+              <BiSun
+                // @ts-ignore
+                className="cursor-pointer text-4xl"
+                onClick={toggleTheme}
+              />
+            )}
+          </section>
         </div>
       </Layout>
     </div>
